f04ed55a22011e6a7bd84ef33227973c906737a6
maruthisundaram <maruthishanmugam7@gmail.com>
2023-06-12 21:41:11 +0530

    Object Oriented Programming

---
+++ src/main/java/Inheritance1_43.java
@@ -0,0 +1,32 @@
+package main.java;
+
+public class Inheritance1_43 {
+    public static void main(String args[]){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
+
+class Bird extends Animal
+{
+    void fly()
+    {
+        System.out.println("I am flying");
+    }
+
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
+
+class Animal{
+    void walk()
+    {
+        System.out.println("I am walking");
+    }
+}
---
+++ src/main/java/Instanceof_49.java
@@ -0,0 +1,39 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Instanceof_49 {
+    static String count(ArrayList mylist){
+        int a = 0,b = 0,c = 0;
+        for(int i = 0; i < mylist.size(); i++){
+            Object element=mylist.get(i);
+            if(element instanceof StudentIns)
+                a++;
+            if(element instanceof Rockstar)
+                b++;
+            if(element instanceof Hacker)
+                c++;
+        }
+        String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+        return ret;
+    }
+
+    public static void main(String []args){
+        ArrayList<Object> mylist = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            String s=sc.next();
+            if(s.equals("Student"))mylist.add(new StudentIns());
+            if(s.equals("Rockstar"))mylist.add(new Rockstar());
+            if(s.equals("Hacker"))mylist.add(new Hacker());
+        }
+        System.out.println(count(mylist));
+        sc.close();
+    }
+}
+
+class StudentIns{}
+class Rockstar{}
+class Hacker{}
